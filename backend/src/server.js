b285f9eaf35c5d97275724c25d4b49602b0947a6
chaotolabin <haibinhthanhbinh@gmail.com>
2025-12-06 10:08:23 +0700

    "create server"

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+// su dung CommonJS module syntax
+// const express = require('express');
+
+// su dung ESM syntax
+import express from 'express';
+
+// Tao ung dung Express
+const app = express();
+
+// Cau hinh cong lang nghe
+// Su dung bien moi truong hoac cong mac dinh 5000, bien moi truong duoc dat trong file .env
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    // In ra thong bao khi server bat dau chay
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.post("/api/tasks", (req, res) => {
+    res.status(201).json({ message: "Task created" });
+});
